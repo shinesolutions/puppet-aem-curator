e975c56e3d7abf9143a9bfc9517cc41fefa6b58a
Michael Bloch <michael.bloch@shinesolutions.com>
2018-03-02 15:05:20 +1100

    Adding new class to create aem-tools for dispatcher

---
+++ manifests/config_aem_tools_dispatcher.pp
@@ -0,0 +1,23 @@
+class aem_curator::config_aem_tools_dispatcher (
+  $base_dir,
+  $docroot_dir,
+) {
+
+  file { "${base_dir}/aem-tools/":
+    ensure => directory,
+    mode   => '0775',
+    owner  => 'root',
+    group  => 'root',
+  } -> file {"${base_dir}/aem-tools/flush-dispatcher-cache.sh":
+    ensure  => present,
+    content => epp('aem_curator/aem-tools/flush-dispatcher-cache.sh.epp', {
+      'base_dir'    => $base_dir
+      'docroot_dir' => $docroot_dir
+      }
+    ),
+    mode    => '0775',
+    owner   => 'root',
+    group   => 'root',
+  }
+
+}
