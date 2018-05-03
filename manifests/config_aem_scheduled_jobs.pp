95819a3aa01eef434fec099629ddc94053c2215c
Michael Bloch <michael.bloch@shinesolutions.com>
2018-05-03 12:48:33 +1000

    Renamed Parameters

--- manifests/config_aem_scheduled_jobs.pp
+++ manifests/config_aem_scheduled_jobs.pp
@@ -1,18 +1,18 @@
 class aem_curator::config_aem_scheduled_jobs (
   $base_dir,
-  $cron_offline_compaction_weekday     = '2',
-  $cron_offline_compaction_hour        = '3',
-  $cron_offline_compaction_minute      = '0',
-  $cron_offline_compaction_enable      = undef
+  $offline_compaction_enable  = false,
+  $offline_compaction_weekday = '2',
+  $offline_compaction_hour    = '3',
+  $offline_compaction_minute  = '0'
 ) {
-  if $cron_offline_compaction_enable {
+  if $offline_compaction_enable {
     cron { 'offline-compaction':
       ensure  => present,
       command => "${base_dir}/aem-tools/offline-compaction.sh >>/var/log/offline-compaction.log 2>&1",
       user    => 'root',
-      weekday => $cron_offline_compaction_weekday,
-      hour    => $cron_offline_compaction_hour,
-      minute  => $cron_offline_compaction_minute,
+      weekday => $offline_compaction_weekday,
+      hour    => $offline_compaction_hour,
+      minute  => $offline_compaction_minute,
     }
   } else {
     cron { 'offline-compaction':
