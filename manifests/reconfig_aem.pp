8aefb15911b86d63566ff0a50ff74032544b1146
Michael Bloch <michael.bloch@shinesolutions.com>
2019-02-08 09:45:58 +1100

    Set force Truststore removal for reconfiguration to false.

--- manifests/reconfig_aem.pp
+++ manifests/reconfig_aem.pp
@@ -74,7 +74,7 @@ define aem_curator::reconfig_aem (
         aem_id       => $aem_id,
         aem_username => $aem_username,
         aem_password => $aem_password,
-        force        => true,
+        force        => false,
         before       => Exec["service aem-${aem_id} stop"],
       }
     }
