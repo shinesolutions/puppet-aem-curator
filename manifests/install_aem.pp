c26d4d190fe3a6b9f8ecd5e4d9c7c3df97b87f90
Timothy.Baert <Timothy.Baert@amplexor.com>
2019-05-10 16:43:27 +0200

    Fix of wrongly named variable.

--- manifests/install_aem.pp
+++ manifests/install_aem.pp
@@ -223,9 +223,9 @@ define aem_curator::install_aem (
     } -> exec { "${aem_id}: Fix repository mount permissions":
       command => "chown -R aem-${aem_id}:aem-${aem_id} ${data_volume_mount_point}",
     } -> exec { "rm -f ${aem_base}/aem/${aem_id}/crx-quickstart/install/aem-healthcheck-content-*.zip":
-    } -> exec { "post start service aem-${aem_name}":
-      command => "/opt/puppetlabs/bin/puppet resource service aem-${aem_name} ensure=running"
-    } -> exec { "${aem_name}: Wait post AEM start":
+    } -> exec { "post start service aem-${aem_id}":
+      command => "/opt/puppetlabs/bin/puppet resource service aem-${aem_id} ensure=running"
+    } -> exec { "${aem_id}: Wait post AEM start":
       command => "sleep 120"
     }
   } else {
@@ -238,9 +238,9 @@ define aem_curator::install_aem (
     } -> exec { "${aem_id}: Ensure AEM resource is stopped":
       command => "/opt/puppetlabs/bin/puppet resource service aem-${aem_id} ensure=stopped",
     } -> exec { "rm -f ${aem_base}/aem/${aem_id}/crx-quickstart/install/aem-healthcheck-content-*.zip":
-    } -> exec { "post start service aem-${aem_name}":
-      command => "/opt/puppetlabs/bin/puppet resource service aem-${aem_name} ensure=running"
-    } -> exec { "${aem_name}: Wait post AEM start":
+    } -> exec { "post start service aem-${aem_id}":
+      command => "/opt/puppetlabs/bin/puppet resource service aem-${aem_id} ensure=running"
+    } -> exec { "${aem_id}: Wait post AEM start":
       command => "sleep 120"
     }
   }
