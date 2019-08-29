1f355617b23c640bc08c8e50a453a6083a6d0c35
Michael Bloch <michael.bloch@shinesolutions.com>
2019-08-29 13:53:08 +1000

    Add installation of aem-healthcheck-content package to provisioning #181

--- manifests/config_author_primary.pp
+++ manifests/config_author_primary.pp
@@ -36,7 +36,7 @@ class aem_curator::config_author_primary (
   $login_ready_max_tries,
   $puppet_conf_dir,
   $tmp_dir,
-  $aem_base                                              = undef,
+  $aem_base                                              = '/opt',
   $aem_healthcheck_source                                = undef,
   $aem_healthcheck_version                               = undef,
   $aem_id                                                = 'author',
@@ -46,6 +46,7 @@ class aem_curator::config_author_primary (
   $enable_aem_reconfiguration                            = false,
   $enable_truststore_migration                           = false,
   $enable_truststore_removal                             = false,
+  $enable_aem_reconfiguratiton_clean_directories         = false,
   $enable_authorizable_keystore_creation                 = false,
   $enable_post_start_sleep                               = false,
   $enable_saml                                           = false,
@@ -75,6 +76,13 @@ class aem_curator::config_author_primary (
   if !defined(File[$tmp_dir]) {
     file { $tmp_dir:
       ensure => directory,
+    }
+  }
+
+  if !defined(File["${tmp_dir}/${aem_id}"]) {
+    file { "${tmp_dir}/${aem_id}":
+      ensure => directory,
+      mode   => '0700',
     }
   }
 
@@ -147,6 +155,41 @@ class aem_curator::config_author_primary (
     exec { "${aem_id}: Cleaning directory ${crx_quickstart_dir}/${clean_directory}/":
       command => "rm -fr ${crx_quickstart_dir}/${clean_directory}/*",
       before  => File["${crx_quickstart_dir}/install/"],
+    }
+  }
+
+  #
+  # If reconfiguration is enabled & clean directories for reconfiguration
+  # is enabled than we don't install AEM Healthcheck because it's already
+  # done during the reconfiguration process. Otherwise install aem healthcheck
+  # apart from the reconfiguration process.
+  #
+  unless $enable_aem_reconfiguratiton_clean_directories {
+  #
+  # remove any aem-healthcheck-content package from the install directory
+  # If install dir isn't cleaned up a step before per default
+  #
+  if !('install' in $list_clean_directories) {
+    exec { "${aem_id}: remove ${crx_quickstart_dir}/install/aem-healthcheck-content-*.zip":
+      command => "rm -fr ${crx_quickstart_dir}/install/aem-healthcheck-content-*.zip",
+      before  => [
+        Service['aem-author']
+      ],
+    }
+  }
+
+  aem_curator::install_aem_healthcheck {"${aem_id}: Install AEM Healthcheck":
+    aem_base                => $aem_base,
+    aem_healthcheck_source  => $aem_healthcheck_source,
+    aem_healthcheck_version => $aem_healthcheck_version,
+    aem_id                  => $aem_id,
+    tmp_dir                 => $tmp_dir,
+    require                 => [
+                                  Service['aem-author'],
+                                ],
+    before                  => [
+                                  Aem_aem["${aem_id}: Wait until login page is ready"],
+                                ]
     }
   }
 
--- manifests/config_publish.pp
+++ manifests/config_publish.pp
@@ -40,33 +40,41 @@ class aem_curator::config_publish (
   $publish_timeout,
   $puppet_conf_dir,
   $tmp_dir,
-  $aem_base                    = undef,
-  $aem_healthcheck_source      = undef,
-  $aem_healthcheck_version     = undef,
-  $aem_id                      = 'publish',
-  $aem_ssl_keystore_password   = undef,
-  $aem_keystore_path           = undef,
-  $cert_base_url               = undef,
-  $data_volume_mount_point     = undef,
-  $delete_repository_index     = false,
-  $enable_aem_reconfiguration  = false,
-  $enable_post_start_sleep     = false,
-  $enable_truststore_creation  = false,
-  $enable_truststore_migration = false,
-  $enable_truststore_removal   = false,
-  $enable_create_system_users  = undef,
-  $post_start_sleep_seconds    = '120',
-  $publish_ssl_port            = undef,
-  $jmxremote_port              = '59183',
-  $jvm_mem_opts                = undef,
-  $jvm_opts                    = undef,
-  $run_mode                    = 'publish',
-  $truststore_password         = undef,
+  $aem_base                                      = '/opt',
+  $aem_healthcheck_source                        = undef,
+  $aem_healthcheck_version                       = undef,
+  $aem_id                                        = 'publish',
+  $aem_ssl_keystore_password                     = undef,
+  $aem_keystore_path                             = undef,
+  $cert_base_url                                 = undef,
+  $data_volume_mount_point                       = undef,
+  $delete_repository_index                       = false,
+  $enable_aem_reconfiguration                    = false,
+  $enable_post_start_sleep                       = false,
+  $enable_truststore_creation                    = false,
+  $enable_truststore_migration                   = false,
+  $enable_truststore_removal                     = false,
+  $enable_aem_reconfiguratiton_clean_directories = false,
+  $enable_create_system_users                    = undef,
+  $post_start_sleep_seconds                      = '120',
+  $publish_ssl_port                              = undef,
+  $jmxremote_port                                = '59183',
+  $jvm_mem_opts                                  = undef,
+  $jvm_opts                                      = undef,
+  $run_mode                                      = 'publish',
+  $truststore_password                           = undef,
 ) {
 
   if !defined(File[$tmp_dir]) {
     file { $tmp_dir:
       ensure => directory,
+    }
+  }
+
+  if !defined(File["${tmp_dir}/${aem_id}"]) {
+    file { "${tmp_dir}/${aem_id}":
+      ensure => directory,
+      mode   => '0700',
     }
   }
 
@@ -139,6 +147,41 @@ class aem_curator::config_publish (
     exec { "${aem_id}: Cleaning directory ${crx_quickstart_dir}/${clean_directory}/":
       command => "rm -fr ${crx_quickstart_dir}/${clean_directory}/*",
       before  => File["${crx_quickstart_dir}/install/"],
+    }
+  }
+
+  #
+  # If reconfiguration is enabled & clean directories for reconfiguration
+  # is enabled than we don't install AEM Healthcheck because it's already
+  # done during the reconfiguration process. Otherwise install aem healthcheck
+  # apart from the reconfiguration process.
+  #
+  unless $enable_aem_reconfiguratiton_clean_directories {
+  #
+  # remove any aem-healthcheck-content package from the install directory
+  # If install dir isn't cleaned up a step before per default
+  #
+  if !('install' in $list_clean_directories) {
+    exec { "${aem_id}: remove ${crx_quickstart_dir}/install/aem-healthcheck-content-*.zip":
+      command => "rm -fr ${crx_quickstart_dir}/install/aem-healthcheck-content-*.zip",
+      before  => [
+        Service['aem-publish']
+      ],
+    }
+  }
+
+  aem_curator::install_aem_healthcheck {"${aem_id}: Install AEM Healthcheck":
+    aem_base                => $aem_base,
+    aem_healthcheck_source  => $aem_healthcheck_source,
+    aem_healthcheck_version => $aem_healthcheck_version,
+    aem_id                  => $aem_id,
+    tmp_dir                 => $tmp_dir,
+    require                 => [
+                                  Service['aem-publish'],
+                                ],
+    before                  => [
+                                  Aem_aem["${aem_id}: Wait until login page is ready"],
+                                ]
     }
   }
 
