cc7ef90b9c93f610552054bf385eb99d93280611
Michael Bloch <michael.bloch@shinesolutions.com>
2019-09-30 16:41:40 +1000

    Fix lint errors #2.

--- manifests/action_upgrade_aem.pp
+++ manifests/action_upgrade_aem.pp
@@ -1,18 +1,19 @@
 class aem_curator::action_upgrade_aem (
   $aem_base,
   $base_dir,
+  $tmp_dir,
   $aem_id                  = $::aem_id,
   $aem_port                = $::aem_port,
   $aem_upgrade_version     = $::aem_upgrade_version,
   $post_upgrade_sleep_secs = $::post_upgrade_sleep_secs,
 ) {
   aem_curator::upgrade_aem { "${aem_id}: Upgrading AEM to version ${aem_upgrade_version}":
-    aem_base                       => $aem_base,
-    aem_id                         => $aem_id,
-    aem_port                       => $aem_port,
-    base_dir                       => $base_dir,
-    post_upgrade_sleep_secs        => $post_upgrade_sleep_secs,
-    tmp_dir                        => $tmp_dir,
-    aem_upgrade_version            => $aem_upgrade_version,
+    aem_base                => $aem_base,
+    aem_id                  => $aem_id,
+    aem_port                => $aem_port,
+    base_dir                => $base_dir,
+    post_upgrade_sleep_secs => $post_upgrade_sleep_secs,
+    tmp_dir                 => $tmp_dir,
+    aem_upgrade_version     => $aem_upgrade_version,
   }
 }
