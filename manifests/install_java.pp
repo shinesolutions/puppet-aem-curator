f39b678079b406146d86b145a2358cebd0b39df8
Cliffano Subagio <cliffano@gmail.com>
2019-10-21 00:58:45 +1100

    Change install_java manifest to download a custom jdk file instead of from oracle.com via Oracle SSO.

--- manifests/install_java.pp
+++ manifests/install_java.pp
@@ -23,12 +23,22 @@
 class aem_curator::install_java (
   $cert_base_url,
   $tmp_dir,
+  $jdk_base_url,
+  $jdk_filename       = 'jdk-8u221-linux-x64.rpm',
+  $jdk_version        = '8',
+  $jdk_version_update = '221',
+  $jdk_version_build  = '',
+  $jdk_format         = 'rpm',
 ) {
 
-  # TODO: will upgrade to >= 8u151 after https://github.com/antoineco/aco-oracle_java/issues/40 is solved
   class { 'oracle_java':
-    version => '8u141',
-    type    => 'jdk',
+    download_url   => $jdk_base_url,
+    filename       => $jdk_filename,
+    version        => "${jdk_version}u${jdk_version_update}",
+    build          => $jdk_version_build,
+    type           => 'jdk',
+    format         => $jdk_format,
+    check_checksum => false,
   }
 
   file { '/etc/ld.so.conf.d/99-libjvm.conf':
