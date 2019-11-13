fa586728b2d80f8130825e803c23269d39f300e4
hoomaan-kh <ho.kheirabadi@gmail.com>
2019-11-13 22:21:16 +1100

    Adding data volume for Author Dispatcher

--- manifests/install_aem.pp
+++ manifests/install_aem.pp
@@ -1,71 +1,36 @@
-# == Class: aem_curator::install_aem
+# == Class: config::author
 #
-#  Shared Resources used by other AEM modules
+# Install AEM and configure for the `publisher` role.
 #
 # === Parameters
-#
-# [*tmp_dir*]
-#   A temporary directory used for staging
-#
-# [*run_modes*]
-#   List of additional AEM run modes
-#
-# [*aem_port*]
-#   TCP port AEM will listen on.
-#
-# [*aem_ssl_port*]
-#   SSL port AEM will listen on.
-#
-# [*aem_artifacts_base*]
-#   URLs (s3://, http:// or file://) for the AEM jar, license and package
-#   files.
-#
-# [*aem_healthcheck_version*]
-#   The version of the AEM healthcheck service to install.
-#
-# [*aem_base*]
-#   Base directory for installing AEM.
-#
-# [*aem_sample_content*]
-#   Boolean that determines whether the AEM sample content should be installed.
-#
-# [*aem_jvm_mem_opts*]
-#   Extra memory options to be passed to the JVM.
-#
-# [*setup_repository_volume*]
-#   Boolean that determines whether a separate volume is formatted and mounted
-#   for the AEM repository.
-#
-# [*data_volume_device*]
-#   The device for format and mount for the AEM repository.
-#
-# [*data_volume_mount_point*]
-#   The mount point for the AEM repository volume.
-#
-# [*aem_keystore_path*]
-#   The full path to the Java keystore that will store the X.509 certificate
-#   and private key to be used by AEM.
-#
-# [*aem_keystore_password*]
-#   The password for the AEM Java keystore.
 #
 # [*cert_base_url*]
 #   Base URL (supported by the puppet-archive module) to download the X.509
 #   certificate and private key to be used with Apache.
 #
+# [*cert_filename*]
+#   The local path and filename to save the X.509 certificate and private key
+#   to be used with Apache.
+#
 # [*tmp_dir*]
 #   A temporary directory used to store the X.509 certificate and private key
 #   while building the PEM file for Apache.
 #
-# [*post_install_sleep_secs*]
-#   Number of seconds to sleep to allow AEM to settle. If installation fails,
-#   try turning this up.
+# [*apache_module_base_url*]
+#   Base URL (supported by the puppet-archive module) to download the archive
+#   containing the AEM Dispatcher Apache module.
 #
-# [*aem_jvm_opts*]
-#   An array of command line options to pass to the JVM when starting AEM.
+# [*apache_module_tarball*]
+#   The name of the archive containing the AEM Dispatcher Apache module.
 #
-# [*aem_cfp_class*]
-# the Puppet class name that used to install an AEM CFP
+# [*apache_module_filename*]
+#   The name of the AEM Dispatcher Apache module shared object file stored
+#   inside the archive.
+#
+# [*apache_module_temp_dir*]
+#   A temporary directory used to store the AEM Dispatcher Apache module while
+#   installing it.
+#
 # === Authors
 #
 # Andy Wang <andy.wang@shinesolutions.com>
@@ -75,166 +40,126 @@
 #
 # Copyright © 2017 Shine Solutions Group, unless otherwise noted.
 #
-
-define aem_curator::install_aem (
-  $aem_license_base,
-  $aem_artifacts_base,
-  $aem_healthcheck_version,
-  $aem_host,
-  $aem_port,
-  $aem_ssl_port,
-  $run_modes,
+class aem_curator::install_dispatcher (
+  $apache_module_base_url,
+  $apache_module_filename,
+  $apache_module_tarball,
+  $apache_module_temp_dir,
+  $cert_base_url,
+  $cert_filename,
   $tmp_dir,
-  $aem_debug_port                = undef,
-  $aem_base                      = '/opt',
-  $aem_debug                     = false,
-  $aem_id                        = undef,
-  $aem_type                      = undef,
-  $aem_jvm_mem_opts              = '-Xss4m -Xmx8192m',
-  $aem_keystore_password         = undef,
-  $aem_keystore_path             = undef,
-  $aem_profile                   = 'aem62_sp1_cfp3',
-  $aem_sample_content            = false,
-  $cert_base_url                 = undef,
-  $aem_jvm_opts                  = [
-    '-XX:+PrintGCDetails',
-    '-XX:+PrintGCTimeStamps',
-    '-XX:+PrintGCDateStamps',
-    '-XX:+PrintTenuringDistribution',
-    '-XX:+PrintGCApplicationStoppedTime',
-    '-XX:+HeapDumpOnOutOfMemoryError',
-  ],
-  $post_install_sleep_secs       = 120,
-  $post_stop_sleep_secs          = 120,
-  $puppet_conf_dir               = '/etc/puppetlabs/puppet/',
-  $data_volume_device            = '/dev/xvdb',
-  $data_volume_mount_point       = '/mnt/ebs1',
-  $retries_base_sleep_seconds    = 10,
-  $retries_max_sleep_seconds     = 10,
-  $retries_max_tries             = 120,
-  $setup_repository_volume       = false,
+  $post_stop_sleep_secs      = 120,
+  $aem_base                  = '/var',
+  $data_volume_device        = '/dev/xvdb',
+  $data_volume_mount_point   = '/mnt/ebs1',
+  $setup_data_volume         = false,
+  $apache_http_port          = '80',
+  $apache_https_port         = '443',
+  $default_vhost             = true,
+  $aem_id                    = 'dispatcher',
 ) {
 
-  if !defined(File[$tmp_dir]) {
-    file { $tmp_dir:
-      ensure => directory,
+    Exec {
+      cwd     => $tmp_dir,
+      path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
+      timeout => 0,
     }
-  }
-  if !defined(File["${tmp_dir}/${aem_id}"]) {
-    file { "${tmp_dir}/${aem_id}":
-      ensure => directory,
-      mode   => '0700',
+
+    if $setup_data_volume {
+    exec { "${aem_id}: Wait for post Dispatcher stop":
+    command => "sleep  ${post_stop_sleep_secs}"
+    } -> exec { "${aem_id}: Prepare device for the AEM Data Volume":
+        command => "mkfs -t ext4 ${data_volume_device}",
+      } -> file { $data_volume_mount_point:
+        ensure => directory,
+        mode   => '0755',
+      } -> mount { $data_volume_mount_point:
+        ensure   => mounted,
+        device   => $data_volume_device,
+        fstype   => 'ext4',
+        options  => 'nofail,defaults,noatime',
+        remounts => false,
+        atboot   => false,
+      }
     }
+
+  # Prepare AEM certificate
+  concat { $cert_filename:
+    mode  => '0600',
+    order => 'numeric',
   }
 
-  Exec {
-    cwd     => $tmp_dir,
-    path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ],
-    timeout => 0,
-  }
-
-  Aem_aem {
-    retries_max_tries          => $retries_max_tries,
-    retries_base_sleep_seconds => $retries_base_sleep_seconds,
-    retries_max_sleep_seconds  => $retries_max_sleep_seconds,
-  }
-
-  if $setup_repository_volume {
-    exec { "${aem_id}: Prepare device for the AEM repository":
-      command => "mkfs -t ext4 ${data_volume_device}",
-    } -> file { $data_volume_mount_point:
-      ensure => directory,
-      mode   => '0755',
-    } -> mount { $data_volume_mount_point:
-      ensure   => mounted,
-      device   => $data_volume_device,
-      fstype   => 'ext4',
-      options  => 'nofail,defaults,noatime',
-      remounts => false,
-      atboot   => false,
-    }
-  }
-
-  if !defined(File["${aem_base}/aem"]) {
-    file { "${aem_base}/aem":
-      ensure => directory,
-      mode   => '0775',
-    }
-  }
-
-  file { "${aem_base}/aem/${aem_id}":
+  file { "${tmp_dir}/${aem_id}":
     ensure => directory,
-    mode   => '0775',
-    owner  => "aem-${aem_id}",
-    group  => "aem-${aem_id}",
+    mode   => '0700',
   }
 
-  aem_resources::puppet_aem_resources_set_config { "${aem_id}: Set puppet-aem-resources config file":
-    conf_dir => $puppet_conf_dir,
-    protocol => 'http',
-    host     => $aem_host,
-    port     => $aem_port,
-    debug    => $aem_debug,
-    aem_id   => $aem_id,
+  [ 'key', 'cert' ].each |$idx, $part| {
+    archive { "${tmp_dir}/${aem_id}/aem.${part}":
+      ensure  => present,
+      source  => "${cert_base_url}/aem.${part}",
+      require => File["${tmp_dir}/${aem_id}"],
+    } -> concat::fragment { "${cert_filename}:${part}":
+      target => $cert_filename,
+      source => "${tmp_dir}/${aem_id}/aem.${part}",
+      order  => $idx,
+    }
   }
 
-  aem_curator::install_aem_profile { "${aem_id}: Install AEM profile ${aem_profile}":
-    aem_artifacts_base      => $aem_artifacts_base,
-    aem_license_base        => $aem_license_base,
-    aem_base                => $aem_base,
-    aem_healthcheck_version => $aem_healthcheck_version,
-    aem_id                  => $aem_id,
-    aem_type                => $aem_type,
-    aem_jvm_mem_opts        => $aem_jvm_mem_opts,
-    aem_port                => $aem_port,
-    aem_debug_port          => $aem_debug_port,
-    aem_profile             => $aem_profile,
-    aem_sample_content      => $aem_sample_content,
-    aem_ssl_port            => $aem_ssl_port,
-    aem_jvm_opts            => $aem_jvm_opts,
-    post_install_sleep_secs => $post_install_sleep_secs,
-    run_modes               => $run_modes,
-    tmp_dir                 => $tmp_dir,
-  } -> aem_curator::config_aem { "${aem_id}: Configure AEM":
-    aem_base              => $aem_base,
-    aem_id                => $aem_id,
-    aem_keystore_password => $aem_keystore_password,
-    aem_keystore_path     => $aem_keystore_path,
-    aem_ssl_port          => $aem_ssl_port,
-    cert_base_url         => $cert_base_url,
-    run_mode              => $aem_id,
-    tmp_dir               => $tmp_dir
+  apache::listen { $apache_http_port: }
+  apache::listen { $apache_https_port: }
+
+  class { '::apache':
+    default_vhost => $default_vhost
+  }
+  $apache_module_classes = [
+    '::apache::mod::ssl',
+    '::apache::mod::headers',
+    '::apache::mod::proxy',
+    '::apache::mod::proxy_http',
+    '::apache::mod::proxy_connect',
+  ]
+  class { $apache_module_classes: }
+
+  archive { $apache_module_tarball:
+    source       => "${apache_module_base_url}/${apache_module_tarball}",
+    path         => "${apache_module_temp_dir}/${apache_module_tarball}",
+    extract      => true,
+    extract_path => $apache_module_temp_dir,
   }
 
-  if $setup_repository_volume {
-    exec { "service aem-${aem_id} stop":
-      require => [
-        Aem_curator::Config_aem["${aem_id}: Configure AEM"],
-        Mount[$data_volume_mount_point],
-      ],
-    } -> exec { "${aem_id}: Wait post AEM stop":
-      command => "sleep ${post_stop_sleep_secs}",
-    } -> exec { "${aem_id}: Ensure AEM resource is stopped":
-      command => "/opt/puppetlabs/bin/puppet resource service aem-${aem_id} ensure=stopped",
-    } -> exec { "mv ${aem_base}/aem/${aem_id} ${data_volume_mount_point}/${aem_id}":
-    } -> exec { "${aem_id}: Set link from ${data_volume_mount_point}/${aem_id} to ${aem_base}/aem/${aem_id}":
-      command => "ln -s ${data_volume_mount_point}/${aem_id} ${aem_base}/aem/${aem_id}",
+  class { '::aem::dispatcher' :
+    module_file => "${apache_module_temp_dir}/${apache_module_filename}",
+  } -> file { '/var/www/html':
+    # Set the Docroot owner and group to apache
+    # https://docs.adobe.com/docs/en/dispatcher/disp-install.html#Apache Web Server - Configure Apache Web Server for Dispatcher
+    ensure => directory,
+    owner  => 'apache',
+    group  => 'apache',
+  } -> tcp_conn_validator { "Ensure dispatcher is listening on http port ${apache_http_port}" :
+    host      => 'localhost',
+    port      => $apache_http_port,
+    try_sleep => 5,
+    timeout   => 60,
+  } -> tcp_conn_validator { "Ensure dispatcher is listening on https port ${apache_https_port}" :
+    host      => 'localhost',
+    port      => $apache_https_port,
+    try_sleep => 5,
+    timeout   => 60,
+   } -> exec { "${aem_id}: Wait post dispatcher stop":
+        command => "sleep ${post_stop_sleep_secs}",
+    } -> exec { "${aem_id}: Ensure dispatcher resource is stopped":
+      command => "/opt/puppetlabs/bin/puppet resource service httpd ensure=stopped",
+    } -> exec { "mv /var/www/html ${data_volume_mount_point}/${aem_id}":
+    } -> exec { "${aem_id}: Set link from ${data_volume_mount_point}/${aem_id} to /var/www/":
+      command => "ln -s ${data_volume_mount_point}/${aem_id} /var/www/html",
       returns => [
         '0'
       ]
-    } -> exec { "${aem_id}: Fix repository mount permissions":
-      command => "chown -R aem-${aem_id}:aem-${aem_id} ${data_volume_mount_point}",
+    } -> exec { "${aem_id}: Fix Data Volume mount permissions":
+      command => "chown -R apache:apache ${data_volume_mount_point}",
+    } -> exec { "${aem_id}: Ensure AEM resource is started":
+      command => "/opt/puppetlabs/bin/puppet resource service httpd ensure=running",
     }
-  } else {
-    exec { "service aem-${aem_id} stop":
-      require => [
-        Aem_curator::Config_aem["${aem_id}: Configure AEM"],
-      ],
-    } -> exec { "${aem_id}: Wait post AEM stop":
-      command => "sleep ${post_stop_sleep_secs}",
-    } -> exec { "${aem_id}: Ensure AEM resource is stopped":
-      command => "/opt/puppetlabs/bin/puppet resource service aem-${aem_id} ensure=stopped",
-    }
-  }
 
 }
