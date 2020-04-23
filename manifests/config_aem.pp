4dcffad9206ce082ceff5dfe2158602600542808
Henry Kuijpers <Henry.Kuijpers@amplexor.com>
2020-04-23 10:37:25 +0200

    Add com.adobe.granite.crxde-support as bundle allowed to be inactive

--- manifests/config_aem.pp
+++ manifests/config_aem.pp
@@ -75,6 +75,7 @@ define aem_curator::config_aem (
     value            => [
       'org.apache.sling.jcr.webdav',
       'org.apache.sling.jcr.davex',
+      'com.adobe.granite.crxde-support',
       'com.adobe.granite.crx-explorer',
       'com.adobe.granite.crxde-lite',
       'com.adobe.acs.acs-aem-commons-bundle-twitter',
