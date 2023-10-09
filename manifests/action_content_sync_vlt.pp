6f138a86f73cac4a36178cab2d702d7a298c86de
mattd-mb <matthew.dingle@shinesolutions.com>
2023-10-09 15:04:13 +1100

    RS-179 Removes security group manipulation from content sync vlt

--- manifests/action_content_sync_vlt.pp
+++ manifests/action_content_sync_vlt.pp
@@ -14,7 +14,6 @@ class aem_curator::action_content_sync_vlt (
   $source_ip                  = $::source_ip,
   $aem_port                   = $::aem_port,
   $content_sync_sg            = $::content_sync_sg,
-  $security_groups            = $::security_groups,
   $deployment_sleep_seconds   = 10,
   $component                  = $::component,
   $source_stack_prefix        = $::source_stack_prefix,
@@ -29,10 +28,6 @@ class aem_curator::action_content_sync_vlt (
   $retries_max_sleep_seconds  = 5,
 ) {
 
-  exec { "${aem_id}: Add security group to allow access to target stack from source stack":
-    command => "aws ec2 modify-instance-attribute --instance-id ${aem_host} --groups ${security_groups} ${content_sync_sg}",
-    path    => '/usr/local/bin/:/bin/',
-  }
   $vlt_rcp_cmd_options = ""
   if $recursive {
     if $exclude_path {
@@ -62,18 +57,12 @@ class aem_curator::action_content_sync_vlt (
     $vlt_rcp_cmd_options = "${vlt_rcp_cmd_options} -n "
   }
 
-
   exec { "${aem_id}: Execute VLT content sync command":
     command => @("CMD"/L),
       /opt/aws-stack-provisioner/aem-tools/${vlt_dir}/vlt rcp ${vlt_rcp_cmd_options} \
       http://${aem_username}:${aem_password}@${source_ip}:${aem_port}/crx/-/jcr:root${content_sync_path} \
       http://${aem_username}:${aem_password}@localhost:${aem_port}/crx/-/jcr:root${content_sync_path},
     | CMD
-    path    => '/usr/local/bin/:/bin/',
-  }
-
-  exec { "${aem_id}: Remove security group to revert access to target stack from source stack":
-    command => "aws ec2 modify-instance-attribute --instance-id ${aem_host} --groups ${security_groups}",
     path    => '/usr/local/bin/:/bin/',
   }
 }
