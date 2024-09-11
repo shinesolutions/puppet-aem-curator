02f477ef4394246aa542a88e83623b80171dfaea
Stephen Shim <ovlords@users.noreply.github.com>
2024-09-11 16:06:49 +1000

    Explicitly set ownership of github workspace directory. This is because release builds were failing with error 'detected dubious ownership' (#287)

--- .github/actions/publish-action/entrypoint.sh
+++ .github/actions/publish-action/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 make clean deps lint package
+chown -R root:root /github/workspace
 make publish "forge_token=${PUPPETFORGE_TOKEN}"
--- .github/actions/release-patch-action/entrypoint.sh
+++ .github/actions/release-patch-action/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git config --global user.email "opensource@shinesolutions.com"
 git config --global user.name "Shine Open Source"
+chown -R root:root /github/workspace
 make release-patch
